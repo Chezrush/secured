904f495cc509ac9c431dcda61d57f63b70d70244
Valentin Miné <valentin.mine@epitech.eu>
2025-01-15 09:33:33 +0100

    feat: 🎸 Add directory include
    Add directory include with inside my.h for the lib, secured.h for the
    mandatory functions and hashtable.h to the return functions for the user

---
+++ include/secured.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** B-CPE-110-PAR-1-1-secured-valentin.mine-perso
+** File description:
+** secured
+*/
+
+#ifndef SECURED_H
+    #define SECURED_H
+
+int secured(void);
+
+#endif /* SECURED_H */
