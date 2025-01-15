eaa91b8276f7c641a6e36add7210b0b5f86630d3
Valentin Miné <valentin.mine@epitech.eu>
2025-01-15 09:41:07 +0100

    feat: 🎸 Add the tests directory
    Adding the test directory with the test_secured.c file

---
+++ tests/test_secured.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2025
+** B-CPE-110-PAR-1-1-secured-valentin.mine-perso
+** File description:
+** test_secured
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "secured.h"
+
+void redirect_all_std(void)
+{
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
+
+int secured(void);
+
+Test(Hello_World, check_return_output, .init = redirect_all_std)
+{
+    secured();
+    cr_assert_stdout_eq_str("Hello World");
+}
