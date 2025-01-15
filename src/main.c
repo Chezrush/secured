c54a1904f40aa7314449effdd7f99e455f21c2be
Valentin Miné <valentin.mine@epitech.eu>
2025-01-15 09:40:05 +0100

    feat: 🎸 Add src directory
    Adding the src directory with the main.c file and the secured.c file

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** B-CPE-110-PAR-1-1-secured-valentin.mine-perso
+** File description:
+** main
+*/
+
+#include "secured.h"
+
+int main(void)
+{
+    return secured();
+}
---
+++ src/secured.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** B-CPE-110-PAR-1-1-secured-valentin.mine-perso
+** File description:
+** secured
+*/
+
+#include <stdio.h>
+
+int secured(void)
+{
+    printf("Hello World");
+    return 0;
+}
