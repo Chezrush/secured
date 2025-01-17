05330c8f65b4f2268088ef3023bb05a90b63e8dd
Valentin Miné <valentin.mine@epitech.eu>
2025-01-17 15:26:58 +0100

    fix: 🐛 add passkey

--- src/hash/hash.c
+++ src/hash/hash.c
@@ -5,7 +5,22 @@
 ** hash
 */
 
+#include <stdio.h>
+
+#include "my.h"
+#include "../../include/secured.h"
+
 int hash(char *key, int len)
 {
-    return 0;
+    int final_hash = 0;
+
+    for (int i = 0; key[i]; ++i) {
+        final_hash += final_hash ^ (key[i] * (i + 1));
+        final_hash += len * my_strlen(key);
+        final_hash <<= key[i] % my_strlen(key);
+        if (final_hash < 0)
+            final_hash *= (-1);
+        /*final_hash >>= my_strlen(pass_key) % my_strlen(key)*/;
+    }
+    return final_hash;
 }
