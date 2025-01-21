00c00f16f69130b18df6ed61c47a72ba546f9ed1
Chloé Belin <chloe.belin@epitech.eu>
2025-01-21 11:50:59 +0100

    feat: 🎸 move linked lists to src

--- src/lib_linked_list/linked_lists.c
+++ src/lib_linked_list/linked_lists.c
@@ -6,11 +6,11 @@
 */
 
 #include "secured.h"
+#include "my.h"
 #include <stdlib.h>
 
 static void free_node(hashtable_value_t *list)
 {
-    free(list->key);
     free(list->value);
     free(list);
 }
@@ -28,21 +28,27 @@ int free_list(hashtable_value_t *list)
 
 int display_list(hashtable_value_t *begin)
 {
-    if (!begin->next) {
+    if (!begin) {
         return SUCCESS;
     }
     if (begin->next == NULL) {
+        my_putstr("\n> ");
+        my_put_nbr(begin->hash_key);
+        my_putstr(" - ");
+        my_putstr(begin->value);
     } else {
         display_list(begin->next);
+        my_putstr("\n> ");
+        my_put_nbr(begin->hash_key);
+        my_putstr(" - ");
+        my_putstr(begin->value);
     }
     return SUCCESS;
 }
 
 static void remove_last_node(hashtable_value_t **list)
 {
-    free((*list)->key);
     free((*list)->value);
-    (*list)->key = NULL;
     (*list)->value = NULL;
 }
 
@@ -58,8 +64,8 @@ static int remove_node(hashtable_value_t **list)
             return FAIL;
         }
         (*list)->id = (*list)->next->id;
-        (*list)->key = my_strdup((*list)->next->key);
         (*list)->value = my_strdup((*list)->next->value);
+        (*list)->hash_key = (*list)->next->hash_key;
         temp->next = (*list)->next;
         (*list)->next = (*list)->next->next;
         free_node(temp->next);
@@ -68,49 +74,49 @@ static int remove_node(hashtable_value_t **list)
     return SUCCESS;
 }
 
-static int delete_next(hashtable_value_t **begin, char *key)
+static int delete_next(hashtable_value_t **begin, int hash_key)
 {
-    if ((*begin)->next->key == key && (*begin)->next->next != NULL) {
+    if ((*begin)->next->hash_key == hash_key && (*begin)->next->next != NULL) {
         if (remove_node(&(*begin)->next) == FAIL) {
             return FAIL;
         }
     }
-    if (!(*begin)->next->next && (*begin)->next->key == key) {
+    if (!(*begin)->next->next && (*begin)->next->hash_key == hash_key) {
         if (remove_node(&(*begin)->next) == FAIL) {
             return FAIL;
         }
         (*begin)->next = NULL;
     }
     if ((*begin)->next != NULL) {
-        delete_in_list(&(*begin)->next, key);
+        delete_in_list(&(*begin)->next, hash_key);
     }
     return SUCCESS;
 }
 
-int delete_in_list(hashtable_value_t **begin, char *key)
+int delete_in_list(hashtable_value_t **begin, int hash_key)
 {
-    if ((*begin)->key == key) {
+    if ((*begin)->hash_key == hash_key) {
         if (remove_node(begin) == FAIL) {
             return FAIL;
         }
     }
     if ((*begin)->next != NULL) {
-        if (delete_next(begin, key) == FAIL) {
+        if (delete_next(begin, hash_key) == FAIL) {
             return FAIL;
         }
     }
     return SUCCESS;
 }
 
-int create_list(hashtable_value_t **list, char *key, char *value, int id)
+int create_list(hashtable_value_t **list, char *value, int id, int hash)
 {
-    (*list)->key = strdup(key);
-    (*list)->value = strdup(value);
+    (*list)->value = my_strdup(value);
+    (*list)->hash_key = hash;
     (*list)->id = id;
     return SUCCESS;
 }
 
-int push_to_list(hashtable_value_t **begin, char *key, char *value, int id)
+int push_to_list(hashtable_value_t **begin, char *value, int id, int hash)
 {
     hashtable_value_t *next = malloc(sizeof(hashtable_value_t));
 
@@ -118,14 +124,13 @@ int push_to_list(hashtable_value_t **begin, char *key, char *value, int id)
         return FAIL;
     }
     next->id = (*begin)->id;
-    next->key = my_strdup((*begin)->key);
     next->value = my_strdup((*begin)->value);
-    free((*begin)->key);
+    next->hash_key = (*begin)->hash_key;
     free((*begin)->value);
     next->next = (*begin)->next;
     (*begin)->next = next;
     (*begin)->id = id;
-    (*begin)->key = my_strdup(key);
     (*begin)->value = my_strdup(value);
+    (*begin)->hash_key = hash;
     return SUCCESS;
 }
