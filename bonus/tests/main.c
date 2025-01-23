d20381432fb7dce93620fe7c735fad0b6bed0c9f
Valentin Miné <valentin.mine@epitech.eu>
2025-01-23 19:39:44 +0100

    fix: 🐛 Add new test into test

--- bonus/tests/main.c
+++ bonus/tests/main.c
@@ -18,16 +18,20 @@ int main(void)
     }
     ht_insert(ht, "Vision", "./Documents/Tournament/Modules/Vision");
     ht_insert(ht, "Kratos", "./Trash/Holidays_Pics/.secret_folder/kratos.ai");
+    ht_dump(ht);
+    printf("\n\n\n");
+    ht_insert(ht, "Kratos", "pipi");
     ht_insert(ht, "<3", "+33 6 31 45 61 23 71");
     ht_insert(ht, "</3", "+33 7 51 49 01 38 11");
     ht_dump(ht);
     ht_delete(ht, "Vision");
     ht_delete(ht, "<3");
-    ht_delete(ht, "Kratos");
+    //ht_delete(ht, "Kratos");
     ht_insert(ht, "Vision", "./Documents/Tournament/Modules/Vision");
     ht_delete(ht, "Vision");
     ht_dump(ht);
     printf("%s\n", ht_search(ht, "</3"));
+    printf("%s\n", ht_search(ht, "Kratos"));
     delete_hashtable(ht);
     return SUCCESS;
 }
