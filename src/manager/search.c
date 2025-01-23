cd3ff47ce32933f70fd47542668a13be49fd027e
Chloé Belin <chloe.belin@epitech.eu>
2025-01-23 15:47:16 +0100

    fix: 🐛 check parameters and return value of hash in ht_search

--- src/manager/search.c
+++ src/manager/search.c
@@ -13,11 +13,14 @@ char *ht_search(hashtable_t *ht, char *key)
 {
     int hash_key = ht->hash(key, ht->len);
     int index = ((hash_key % ht->len) + ht->len) % ht->len;
-    hashtable_entry_t *entry = ht->list[index];
-    hashtable_value_t *value = entry->list;
+    hashtable_entry_t *entry = NULL;
+    hashtable_value_t *value = NULL;
 
-    if (hash_key < 0)
-        hash_key *= -1;
+    if (key[0] == '\0' || hash_key < 0) {
+        return FAIL;
+    }
+    entry = ht->list[index];
+    value = entry->list;
     for (int i = 0; i < entry->num_item; ++i) {
         if (value->hash_key == hash_key) {
             return value->value;
