e7acda6d1aea8e96841a7c5ecfb5f36351e22c3d
Valentin Miné <valentin.mine@epitech.eu>
2025-01-23 19:40:34 +0100

    fix: 🐛 Modify name of value -> ht_value

--- src/manager/search.c
+++ src/manager/search.c
@@ -14,18 +14,18 @@ char *ht_search(hashtable_t *ht, char *key)
     int hash_key = ht->hash(key, ht->len);
     int index = ((hash_key % ht->len) + ht->len) % ht->len;
     hashtable_entry_t *entry = NULL;
-    hashtable_value_t *value = NULL;
+    hashtable_value_t *ht_value = NULL;
 
     if (key[0] == '\0' || hash_key < 0) {
         return NULL;
     }
     entry = ht->list[index];
-    value = entry->list;
+    ht_value = entry->list;
     for (int i = 0; i < entry->num_item; ++i) {
-        if (value->hash_key == hash_key) {
-            return value->value;
+        if (ht_value->hash_key == hash_key) {
+            return ht_value->value;
         }
-        value = value->next;
+        ht_value = ht_value->next;
     }
     return NULL;
 }
