02d21d1727789c396aff1009b2e600bfe0eec766
Chloé Belin <chloe.belin@epitech.eu>
2025-01-23 15:44:11 +0100

    fix: 🐛 check parameters in hash function

--- src/hash/hash.c
+++ src/hash/hash.c
@@ -15,10 +15,16 @@ int hash(char *key, int len)
     const char *message = key;
     uint8_t hash[32];
     SHA256_CTX_t ctx = {};
+    int hash_key = 0;
 
-    (void)len;
+    if (key[0] == '\0' || len <= 0) {
+        return -1;
+    }
     sha256_init(&ctx);
     sha256_update(&ctx, (uint8_t *)message, my_strlen(message));
     sha256_final(&ctx, hash);
-    return convert_chunk(hash);
+    hash_key = convert_chunk(hash);
+    if (hash_key < 0)
+        hash_key *= -1;
+    return hash_key;
 }
