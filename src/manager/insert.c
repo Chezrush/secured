b5a8674cc91e8edea47c3a2ace888cf16c6aec4b
Chloé Belin <chloe.belin@epitech.eu>
2025-01-24 18:13:52 +0100

    fix: 🐛 num item incrementation

--- src/manager/insert.c
+++ src/manager/insert.c
@@ -36,12 +36,8 @@ static hashtable_entry_t *search_value(hashtable_t *ht, char *key)
     return NULL;
 }
 
-static int insert_value(hashtable_entry_t **entry, hashtable_entry_t *entry_s,
-    char *value, int hash_key)
+static int insert_value(hashtable_entry_t **entry, char *value, int hash_key)
 {
-    if (entry_s) {
-        return replace_value(entry_s, value);
-    }
     if ((*entry)->num_item == 0) {
         return create_list(&(*entry)->list, value, (*entry)->num_item,
             hash_key);
@@ -59,17 +55,17 @@ int ht_insert(hashtable_t *ht, char *key, char *value)
     hashtable_entry_t *entry_s = NULL;
     int return_value;
 
-    if (!ht || key[0] == '\0' || value[0] == '\0') {
+    if (!ht || key[0] == '\0' || value[0] == '\0')
         return FAIL;
-    }
     hash_key = ht->hash(key, ht->len);
-    if (hash_key < 0) {
+    if (hash_key < 0)
         return FAIL;
-    }
     index = ((hash_key % ht->len) + ht->len) % ht->len;
     entry = &(ht->list[index]);
     entry_s = search_value(ht, key);
-    return_value = insert_value(entry, entry_s, value, hash_key);
+    if (entry_s)
+        return replace_value(entry_s, value);
+    return_value = insert_value(entry, value, hash_key);
     (*entry)->num_item += 1;
     return return_value;
 }
