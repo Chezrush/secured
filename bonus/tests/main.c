8008396f8572f9fffb3633fc3d5c724b76c56432
Chloé Belin <chloe.belin@epitech.eu>
2025-01-24 18:29:55 +0100

    feat: 🎸 new main tests for debugging

--- bonus/tests/main.c
+++ bonus/tests/main.c
@@ -16,6 +16,7 @@ int main(void)
     if (!ht) {
         return FAIL;
     }
+    printf("insert\n");
     printf("vision\n");
     ht_insert(ht, "Vision", "./Documents/Tournament/Modules/Vision");
     printf("kratos 1\n");
@@ -23,23 +24,32 @@ int main(void)
     printf("kratos 2\n");
     ht_insert(ht, "Kratos", "pipi");
     printf("<3\n");
-    ht_dump(ht);
     ht_insert(ht, "<3", "+33 6 31 45 61 23 71");
     printf("</3\n");
     ht_insert(ht, "</3", "+33 7 51 49 01 38 11");
     printf("dump\n");
     ht_dump(ht);
+    printf("\n\ndelete\n");
+    printf("vision\n");
     ht_delete(ht, "Vision");
+    printf("<3\n");
     ht_delete(ht, "<3");
+    printf("insert vision\n");
     ht_insert(ht, "Vision", "./Documents/Tournament/Modules/Vision");
+    printf("delete vision\n");
     ht_delete(ht, "Vision");
+    printf("dump\n");
     ht_dump(ht);
+    printf("delete zuefdsnj\n");
     ht_delete(ht, "zuefdsnj");
     printf("\n");
     ht_dump(ht);
     printf("\n");
+    printf("delete </3\n");
     ht_delete(ht, "</3");
-    printf("%s\n", ht_search(ht, "</3"));
+    printf("\nsearch </3\n");
+    printf("%d\n", ht_search(ht, "</3") == NULL);
+    printf("search kratos\n");
     printf("%s\n", ht_search(ht, "Kratos"));
     delete_hashtable(ht);
     return SUCCESS;
