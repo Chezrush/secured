0b196ce6d04096f246f48720dd488059aadd820d
Chloé Belin <chloe.belin@epitech.eu>
2025-01-24 17:53:05 +0100

    fix: 🐛 check ht parameter

--- src/dump/dump.c
+++ src/dump/dump.c
@@ -9,22 +9,24 @@
 #include "secured.h"
 #include "my.h"
 
-static void display_entry(hashtable_entry_t *entry)
+static void display_entry(hashtable_t *ht, int i)
 {
-    if (entry->num_item != 0) {
-        display_list(entry->list);
+    if (ht->list[i]->num_item != 0) {
+        if (ht->list[i]->num_item != 0) {
+            display_list(ht->list[i]->list);
+        }
     }
 }
 
 void ht_dump(hashtable_t *ht)
 {
-    for (int i = 0; i < ht->len; ++i) {
-        my_putchar('[');
-        my_put_nbr(i);
-        my_putstr("]:");
-        if (ht->list[i]->num_item != 0) {
-            display_entry(ht->list[i]);
+    if (ht) {
+        for (int i = 0; i < ht->len; ++i) {
+            my_putchar('[');
+            my_put_nbr(i);
+            my_putstr("]:");
+            display_entry(ht, i);
+            my_putchar('\n');
         }
-        my_putchar('\n');
     }
 }
